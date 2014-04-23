4022a70e2e804b42f995c9f705d1dce43551c70a
Fred <fred@deliveryhero.com>
2014-04-23 19:59:24 +0200

    Initial commit

---
+++ AlbumSample/src/main/java/com/sample/album/AlbumSampleMain.java
@@ -0,0 +1,52 @@
+package com.sample.album;
+
+import android.app.Activity;
+
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.widget.DrawerLayout;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.sample.album.common.ItemsList;
+
+
+public class AlbumSampleMain extends Activity
+        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+
+    private NavigationDrawerFragment mNavigationDrawerFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_album_sample_main);
+
+        mNavigationDrawerFragment = (NavigationDrawerFragment)
+                getFragmentManager().findFragmentById(R.id.navigation_drawer);
+
+        // Set up the drawer.
+        mNavigationDrawerFragment.setUp(
+                R.id.navigation_drawer,
+                (DrawerLayout) findViewById(R.id.drawer_layout));
+    }
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        FragmentManager fragmentManager = getFragmentManager();
+
+        ItemsList itemsList = new ItemsList();
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, itemsList)
+                .commit();
+    }
+}
---
+++ AlbumSample/src/main/java/com/sample/album/common/Item.java
@@ -0,0 +1,22 @@
+package com.sample.album.common;
+
+public class Item {
+    private String mainHeader;
+    private String secondaryHeader;
+
+    public String getMainHeader() {
+        return mainHeader;
+    }
+
+    public void setMainHeader(String mainHeader) {
+        this.mainHeader = mainHeader;
+    }
+
+    public String getSecondaryHeader() {
+        return secondaryHeader;
+    }
+
+    public void setSecondaryHeader(String secondaryHeader) {
+        this.secondaryHeader = secondaryHeader;
+    }
+}
